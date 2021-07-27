c2669adfe80e498c55ee729c8a202f06c8354d5d
Eenjoi <Andrew.Kowalski18@gmail.com>
2021-07-27 17:35:50 -0500

    removed files

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
-
  
  const particlesOptions = {
    particles: {
